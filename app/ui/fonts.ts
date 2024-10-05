1fc671f92c6242486b107ab1ed35ea34a30aa547
GuillermoTorrez <guillermo.torrez@gmail.com>
2024-10-05 12:18:40 -0400

    Server Component Update

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Onest } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const onest = Onest({ subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: ['400', '700'], style: 'normal', subsets: ['latin'] })
